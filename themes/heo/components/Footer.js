b5ba281ebd2ea3748f23a12068af2686782ce724
文心 <121679359+LineXic@users.noreply.github.com>
2024-07-03 13:56:24 +0800

    更新 Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -25,63 +25,6 @@ const Footer = ({ title }) => {
 
       {/* 底部页面信息 */}
 
-
-<img src="https://icp.gov.moe/images/ico64.png" width="20" height="20"><a href="https://icp.gov.moe/?keyword=20240181" target="_blank">萌ICP备20240181号</a><br>
-<span id="sitetime"></span>
-<script language=javascript>
-    function siteTime(){
-        window.setTimeout("siteTime()", 1000);
-        var seconds = 1000;
-        var minutes = seconds * 60;
-        var hours = minutes * 60;
-        var days = hours * 24;
-        var years = days * 365;
-        var today = new Date();
-        var todayYear = today.getFullYear();
-        var todayMonth = today.getMonth()+1;
-        var todayDate = today.getDate();
-        var todayHour = today.getHours();
-        var todayMinute = today.getMinutes();
-        var todaySecond = today.getSeconds();
-        /* Date.UTC() -- 返回date对象距世界标准时间(UTC)1970年1月1日午夜之间的毫秒数(时间戳)
-        year - 作为date对象的年份，为4位年份值
-        month - 0-11之间的整数，做为date对象的月份
-        day - 1-31之间的整数，做为date对象的天数
-        hours - 0(午夜24点)-23之间的整数，做为date对象的小时数
-        minutes - 0-59之间的整数，做为date对象的分钟数
-        seconds - 0-59之间的整数，做为date对象的秒数
-        microseconds - 0-999之间的整数，做为date对象的毫秒数 */
-        var t1 = Date.UTC(2023,09,02,00,00,00); //北京时间2016-12-1 00:00:00
-        var t2 = Date.UTC(todayYear,todayMonth,todayDate,todayHour,todayMinute,todaySecond);
-        var diff = t2-t1;
-        var diffYears = Math.floor(diff/years);
-        var diffDays = Math.floor((diff/days)-diffYears*365);
-        var diffHours = Math.floor((diff-(diffYears*365+diffDays)*days)/hours);
-        var diffMinutes = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours)/minutes);
-        var diffSeconds = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours-diffMinutes*minutes)/seconds);
-        document.getElementById("sitetime").innerHTML="匍匐爬行"+diffYears+" 年 "+diffDays+" 天 "+diffHours+" 小时 "+diffMinutes+" 分钟 "+diffSeconds+" 秒";
-    }
-    siteTime();
-</script>
-<br>
-
-
-                    Copyright &copy; {{ site.title }} {{ site.time | date: '%Y' }}
-                    <br>
-                    Power by <a href="https://linexic.top/">LineXic</a> | 站点 <a href="https://linexic.top/feed.xml">RSS</a><br>
-                    <a href="https://blog.csdn.net/m0_73230319?">
-  <img width="25" src="https://ts1.cn.mm.bing.net/th/id/R-C.2a932441dd0f785a3d2813c6198998dd?rik=yW78wW2nkdrw8Q&riu=http%3a%2f%2fpic.2265.com%2fupload%2f2017-7%2f201772598294888.png&ehk=ZyTimiwbIKnzPImq7nY8plTTZKjQtBktmyUef%2b4WI4U%3d&risl=&pid=ImgRaw&r=0"alt="CSDN"  />
-</a>
-||
- <a href="http://afdian.net/a/LineXic">
-  <img width="25" src="https://tse1-mm.cn.bing.net/th/id/OIP-C.P7Tlj6hqpuSbLQ1AqQ8mkQHaHa?dpr=3&pid=ImgDetMain"alt="爱发电"  />
-</a>
-||
-<a href="https://space.bilibili.com/1015205011">
-  <img width="25" src="https://ts1.cn.mm.bing.net/th/id/R-C.5f7657549f4bd16d08b56ccad794f01a?rik=%2bXBzOkKWIBMU8A&riu=http%3a%2f%2fwww.kuaipng.com%2fUploads%2fpic%2fw%2f2020%2f07-20%2f89190%2fwater_89190_698_698_.png&ehk=b6FaBHz%2bTBstG7fZg%2fGSVXCcCSqOw3pUcMIZr6QUdZg%3d&risl=&pid=ImgRaw&r=0"alt="bilibili"  />
-</a>
-
-
       <div
         id='footer-bottom'
         className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
